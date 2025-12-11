005908f96c5afb8d2ed96c0dfbbce9442baf1d67
linahousni8-cmd <linahousni8@gmail.com>
2025-12-11 19:46:33 +0100

    une autre modification de html et ts config et j ai laisse ./ comme base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cd-MedQCM-Pro/', // Assure que les chemins sont relatifs pour GitHub Pages
+  base: './', // Important pour GitHub Pages
   build: {
     outDir: 'dist',
   }
